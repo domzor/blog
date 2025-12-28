72cf7d7cdc63119b3a24c5caeb86a1bc04391606
Dom van Blerk <hexagonalbanana@gmail.com>
2025-12-28 12:37:54 +0200

    github pages (hopefully)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -29,6 +29,8 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "domzor", // Usually your GitHub org/user name.
   projectName: "blog", // Usually your repo name.
+  // deploymentBranch: "gh-pages",
+  trailingSlash: false,
 
   onBrokenLinks: "throw",
 
